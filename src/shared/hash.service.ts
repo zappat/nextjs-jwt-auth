0fdb27dcc7a13e8a039935d170464a77721c3dc7
zappat <zappat9@gmail.com>
2023-03-28 13:33:03 +0000

    refactor the product controller

--- src/shared/hash.service.ts
+++ src/shared/hash.service.ts
@@ -1,16 +1,14 @@
-import {
-    Injectable
-  } from '@nestjs/common';
-  import * as bcrypt from 'bcryptjs';
-  
-  @Injectable()
-  export class HashService {
-    async hashPassword(password: string) {
-      const saltOrRounds = 10;
-      return await bcrypt.hash(password, saltOrRounds);
-    }
-  
-    async comparePassword(password: string, hash) {
-      return await bcrypt.compare(password, hash)
-    }
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class HashService {
+  async hashPassword(password: string) {
+    const saltOrRounds = 10;
+    return await bcrypt.hash(password, saltOrRounds);
   }
+
+  async comparePassword(password: string, hash) {
+    return await bcrypt.compare(password, hash);
+  }
+}
