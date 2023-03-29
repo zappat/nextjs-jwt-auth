2d32b03d88d375c1fce55191b8065400be65a998
zappat <zappat9@gmail.com>
2023-03-29 09:36:07 +0000

    implement get all categories

--- src/shared/base.model.ts
+++ src/shared/base.model.ts
@@ -15,12 +15,12 @@ export class BaseModel extends Typegoose {
 
 export const schemaOptions: SchemaOptions = {
   toJSON: {
-    versionKey: false,    
+    versionKey: false,
     virtuals: true,
     getters: true,
     transform: function (doc, ret, options) {
       ret.id = ret._id;
-      delete ret._id;      
+      delete ret._id;
     },
   },
   timestamps: true,
